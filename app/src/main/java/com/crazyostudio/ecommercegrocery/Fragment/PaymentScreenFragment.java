b25780ffc4731b25b82346eb686ee37e3f22a273
ronak <r7023j@gmail.com>
2024-03-29 14:01:18 +0530

    before changing database

--- app/src/main/java/com/crazyostudio/ecommercegrocery/Fragment/PaymentScreenFragment.java
+++ app/src/main/java/com/crazyostudio/ecommercegrocery/Fragment/PaymentScreenFragment.java
@@ -12,8 +12,10 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.crazyostudio.ecommercegrocery.Activity.OrderDetailsActivity;
+import com.crazyostudio.ecommercegrocery.Adapter.OrderProductAdapter;
 import com.crazyostudio.ecommercegrocery.Model.AddressModel;
 import com.crazyostudio.ecommercegrocery.Model.OrderModel;
 import com.crazyostudio.ecommercegrocery.Model.ProductModel;
@@ -21,6 +23,7 @@ import com.crazyostudio.ecommercegrocery.Model.UserinfoModels;
 import com.crazyostudio.ecommercegrocery.R;
 import com.crazyostudio.ecommercegrocery.Services.AuthService;
 import com.crazyostudio.ecommercegrocery.databinding.FragmentPaymentScreenBinding;
+import com.crazyostudio.ecommercegrocery.interfaceClass.OrderProductInterface;
 import com.crazyostudio.ecommercegrocery.javaClasses.basicFun;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -34,13 +37,13 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Objects;
 
-public class PaymentScreenFragment extends Fragment {
+public class PaymentScreenFragment extends Fragment implements OrderProductInterface {
     private FragmentPaymentScreenBinding binding;
     private AddressModel adders;
     private UserinfoModels userinfoModels;
     private String PaymentMode;
     private String addersMode;
-    private String id;
+//    private String id;
     private String BuyType;
     private String CouponCode;
     Cart cart;
@@ -53,6 +56,13 @@ public class PaymentScreenFragment extends Fragment {
     BigDecimal getTotalPrice;
     double newCouponValue = 0; // Variable to hold the applied coupon value
     private boolean CouponValueIsApply = false;
+    ArrayList<ProductModel> ShowProductModels;
+
+
+    private static final String RupeeSymbols = "₹";
+    private static final int STANDARD = 30;
+    private static final int FAST_DELIVERY = 50;
+
     public PaymentScreenFragment(){}
 
     @SuppressLint("SetTextI18n")
@@ -61,7 +71,7 @@ public class PaymentScreenFragment extends Fragment {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
             BuyType = getArguments().getString("BuyType");
-            id = getArguments().getString("id");
+//            id = getArguments().getString("id");
             adders = getArguments().getParcelable("adders");
             userinfoModels = getArguments().getParcelable("user");
             if (BuyType.equals("Now")) {
@@ -84,7 +94,11 @@ public class PaymentScreenFragment extends Fragment {
         PaymentMode = "";
         addersMode = "";
         firebaseDatabase = FirebaseDatabase.getInstance();
+        ShowProductModels = new ArrayList<>();
+        binding.progressCircular.setVisibility(View.VISIBLE);
+        getProduct();
         getTotalPrice = BigDecimal.ZERO;
+
         if (BuyType.equals("Now")) {
             getTotalPrice = BigDecimal.valueOf(productModel.getPrice() * productModel.getDefaultQuantity());
         }
@@ -96,53 +110,80 @@ public class PaymentScreenFragment extends Fragment {
         binding.couponCodeApplyBtn.setOnClickListener(view->{
             if (!CouponValueIsApply) {
                 CouponCode = binding.couponCode.getEditText().getText().toString();
-                if (CouponCode.equals("hello")) {
-//                 Apply coupon value if coupon code is valid
-                    CouponValueIsApply = true;
-                    newCouponValue = 50; // Example coupon value
-                    binding.couponCodeApplyBtn.setText("remove");
-                    binding.couponCode.setEnabled(!CouponValueIsApply);
-                }
-                else {
-                    CouponValueIsApply = false;
-                    newCouponValue = 0; // No coupon applied
-                    binding.couponCodeApplyBtn.setText("Apply");
-                    binding.couponCode.setEnabled(!CouponValueIsApply);
-                }
-                total = total - newCouponValue;
-                binding.TotalPrice.setText("₹" + total);
+                firebaseDatabase.getReference().child("admin").child("Coupon").child(CouponCode).addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if (snapshot.exists()) {
+                            // Coupon code exists, retrieve discount amount and min value
+                            String couponDiscountAmount = snapshot.child("coupon_discount_amount").getValue(String.class);
+                            String couponMinValue = snapshot.child("couponMinvalue").getValue(String.class);
+                            assert couponMinValue != null;
+                            double couponMin = Double.parseDouble(couponMinValue);
+                            assert couponDiscountAmount != null;
+                            if (Subtotal >= couponMin) {
+                                // Subtotal is not less than couponMinValue
+                                // Proceed with your logic here
+                                CouponValueIsApply = true;
+                                newCouponValue = Double.parseDouble(couponDiscountAmount); // Example coupon value
+                                binding.couponCodeApplyBtn.setText("remove");
+                                binding.discount.setText(""+newCouponValue);
+                                binding.couponCode.setEnabled(!CouponValueIsApply);
+                                total = total - newCouponValue;
+                                binding.TotalPrice.setText(RupeeSymbols + total);
+                            } else {
+                                binding.couponCode.setError("Minimum subtotal value required to apply the coupon is "+RupeeSymbols + couponMinValue);
+                            }
+
+                        } else {
+                            // Coupon code does not exist
+                            binding.couponCode.setError(CouponCode + " not valid");
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                        // Handle database error
+                        binding.couponCode.setError(CouponCode + " not valid");
+                        CouponValueIsApply = false;
+                        newCouponValue = 00; // No coupon applied
+                        binding.couponCodeApplyBtn.setText("Apply");
+                        binding.discount.setText(""+newCouponValue);
+                        binding.couponCode.setEnabled(!CouponValueIsApply);
+                    }
+                });
             }
             else {
                 total = total + newCouponValue;
-                binding.TotalPrice.setText("₹" + total);
+                binding.TotalPrice.setText(RupeeSymbols + total);
                 newCouponValue = 0; // No coupon applied
                 CouponValueIsApply = false;
+                binding.discount.setText(""+newCouponValue);
                 binding.couponCodeApplyBtn.setText("Apply");
                 binding.couponCode.setEnabled(!CouponValueIsApply);
             }
-
         });
         Subtotal = Double.parseDouble(getTotalPrice.toString());
         BigDecimal threshold = new BigDecimal("150.0"); // Define the threshold as a BigDecimal
         int comparisonResult = getTotalPrice.compareTo(threshold);
         if (comparisonResult < 0) {
             isShippingCost = true;
-            binding.radiaId1.setText("₹50 Standard delivery if your buy more than 150 free delivery");
-            binding.SubTotalPrice.setText("₹"+getTotalPrice);
-            binding.ShippingPrice.setText("₹50");
+            binding.radiaId1.setText(RupeeSymbols+"50 Standard delivery if your buy more than 150 free delivery");
+            binding.SubTotalPrice.setText(RupeeSymbols+getTotalPrice);
+            binding.ShippingPrice.setText(RupeeSymbols + "50");
             ShippingFee = 50;
             addersMode = "Standard delivery";
             total = Double.parseDouble(getTotalPrice.toString())+ShippingFee-newCouponValue;
-            binding.TotalPrice.setText("₹"+total);
+            binding.TotalPrice.setText(RupeeSymbols+total);
             // totalPrice is less than 150
             // Add your logic here
         } else {
-            binding.SubTotalPrice.setText("₹"+getTotalPrice);
+            binding.SubTotalPrice.setText(RupeeSymbols+getTotalPrice);
             binding.ShippingPrice.setText("0");
             ShippingFee = 0;
             addersMode = "Standard delivery";
             total = Double.parseDouble(getTotalPrice.toString())-newCouponValue;
-            binding.TotalPrice.setText("₹"+total);
+
+            binding.TotalPrice.setText(RupeeSymbols+total);
 
         }
         binding.payment.setOnCheckedChangeListener((radioGroup, i) -> {
@@ -159,29 +200,29 @@ public class PaymentScreenFragment extends Fragment {
             RadioButton radioButton = requireActivity().findViewById(i);
             if (radioButton.getId()==R.id.radia_id1){
                 if (isShippingCost) {
-                    binding.SubTotalPrice.setText("₹"+getTotalPrice);
-                    binding.ShippingPrice.setText("₹50");
+                    binding.SubTotalPrice.setText(RupeeSymbols+getTotalPrice);
+                    binding.ShippingPrice.setText(RupeeSymbols+"50");
                     ShippingFee = 50;
                     addersMode = "Standard delivery";
                     total = Double.parseDouble(String.valueOf(getTotalPrice))+ShippingFee-newCouponValue;
-                    binding.TotalPrice.setText("₹"+total);
+                    binding.TotalPrice.setText(RupeeSymbols+total);
 
                 }else {
-                    binding.SubTotalPrice.setText("₹"+getTotalPrice);
-                    binding.ShippingPrice.setText("₹0");
+                    binding.SubTotalPrice.setText(RupeeSymbols+getTotalPrice);
+                    binding.ShippingPrice.setText(RupeeSymbols+"0");
                     ShippingFee = 0;
                     addersMode = "Standard delivery";
                     total = Double.parseDouble(getTotalPrice.toString())-newCouponValue;
-                    binding.TotalPrice.setText("₹"+total);
+                    binding.TotalPrice.setText(RupeeSymbols+total);
                 }
             }
             else {
-                binding.SubTotalPrice.setText("₹"+getTotalPrice);
-                binding.ShippingPrice.setText("₹60");
+                binding.SubTotalPrice.setText(RupeeSymbols+getTotalPrice);
+                binding.ShippingPrice.setText(RupeeSymbols+"60");
                 addersMode = " Fast delivery";
                 ShippingFee = 60;
                 total = Double.parseDouble(String.valueOf(getTotalPrice))+ShippingFee - newCouponValue;
-                binding.TotalPrice.setText("₹"+total);
+                binding.TotalPrice.setText(RupeeSymbols+total);
             }
         });
         binding.Change.setOnClickListener(Change-> requireActivity().onBackPressed());
@@ -206,6 +247,27 @@ public class PaymentScreenFragment extends Fragment {
             }
         });
         return binding.getRoot();
+    }
+
+    private void getProduct() {
+        firebaseDatabase.getReference().child("Cart").child(Objects.requireNonNull(FirebaseAuth.getInstance().getUid())).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot snapshot1 : snapshot.getChildren()) {
+                    ProductModel productModel = snapshot1.getValue(ProductModel.class);
+                    if (productModel != null) {
+                        ShowProductModels.add(productModel);
+
+                    }
+                }
+                ShowProduct();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     private void PlaceOrder(String PaymentMode, UserinfoModels userinfoModels, long time) {
@@ -299,4 +361,17 @@ public class PaymentScreenFragment extends Fragment {
 
     }
 
+    @SuppressLint("NotifyDataSetChanged")
+    private void ShowProduct(){
+        OrderProductAdapter orderProductAdapter = new OrderProductAdapter(ShowProductModels,requireContext(),this);
+        binding.ProductView.setAdapter(orderProductAdapter);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(requireContext(),LinearLayoutManager.VERTICAL,false);
+        binding.ProductView.setLayoutManager(layoutManager);
+        orderProductAdapter.notifyDataSetChanged();
+        binding.progressCircular.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void onOrder(ProductModel model) {
+    }
 }
